36d3f78ad3c25ac1ba241d960fd6bad5d265f203
Noah laptop <magic.monkey.ark@gmail.com>
2020-08-19 13:21:09 -0500

    Added Board, GameState, and Tile

---
+++ src/Board/Board.java
@@ -0,0 +1,18 @@
+package Board;
+
+import java.awt.*;
+
+public class Board {
+
+    public Board(int x, int y, int tileWidth, int tileHeight, int numTile) {
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
---
+++ src/States/GameState.java
@@ -0,0 +1,23 @@
+package States;
+
+import Handler.Handler;
+
+import java.awt.*;
+
+public class GameState extends State {
+
+    public GameState(Handler handler) {
+        super(handler);
+    }
+
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
