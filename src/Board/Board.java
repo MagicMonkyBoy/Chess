713b860f86b14e0ae3d3e6e6c53ebfd8bcba63f7
MagicMonkyBoy <44240860+MagicMonkyBoy@users.noreply.github.com>
2020-08-20 00:01:36 -0500

    Tiny changes

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -4,7 +4,15 @@ import java.awt.*;
 
 public class Board {
 
-    public Board(int x, int y, int tileWidth, int tileHeight, int numTile) {
+    private int x, y, tileWidth, tileHeight, numTileWidth, numTileHeight;
+
+    public Board(int x, int y, int tileWidth, int tileHeight, int numTileWidth, int numTileHeight) {
+        this.x = x;
+        this.y = y;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.numTileWidth = numTileWidth;
+        this.numTileHeight = numTileHeight;
 
     }
 
